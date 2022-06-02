d5f04e50723e44d942201b8eb8d8418fba2ab52a
Diego Matera <diegomatera@gmail.com>
2022-06-02 13:07:31 -0300

    correccion form

--- src/js/contat-form.js
+++ src/js/contat-form.js
@@ -1,19 +1,27 @@
 const $form = document.querySelector("#form");
+const $form2 = document.querySelector("#form2");
+
 const $headerForm = document.querySelector("#header-form");
 const $regaloForm = document.querySelector("#regalo-form");
 
 const d = document;
 
 $form.addEventListener("submit", handleSumbit);
+$form2.addEventListener("submit", handleSumbit);
+
 $headerForm.addEventListener("submit", handleSumbit2);
 $regaloForm.addEventListener("submit", handleSumbit2);
 
 function handleSumbit(event) {
   event.preventDefault();
   const $resp = d.querySelector("#response");
+  const $resp2 = d.querySelector("#response2");
+
   const $contactLoader = d.querySelector("#contact-loader");
+  const $contactLoader2 = d.querySelector("#contact-loader2");
 
   $contactLoader.classList.remove("none");
+  $contactLoader2.classList.remove("none");
 
   fetch("https://formsubmit.co/ajax/diegomatera@gmail.com", {
     method: "POST",
@@ -23,8 +31,11 @@ function handleSumbit(event) {
     .then((json) => {
       console.log(json);
       $contactLoader.classList.add("none");
+      $contactLoader2.classList.add("none");
       $resp.classList.remove("none");
+      $resp2.classList.remove("none");
       $form.reset();
+      $form2.reset();
     })
     .catch((err) => {
       console.log(err);
