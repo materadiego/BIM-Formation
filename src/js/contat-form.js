4352f62735acddca2a4a4c116bd13f442d03111b
Diego Matera <diegomatera@gmail.com>
2022-06-20 23:34:30 -0300

    Proyecto terminado

--- src/js/contat-form.js
+++ src/js/contat-form.js
@@ -16,46 +16,72 @@ function handleSumbit(event) {
   event.preventDefault();
   const $resp = d.querySelector("#response");
   const $resp2 = d.querySelector("#response2");
-
   const $contactLoader = d.querySelector("#contact-loader");
   const $contactLoader2 = d.querySelector("#contact-loader2");
 
   $contactLoader.classList.remove("none");
   $contactLoader2.classList.remove("none");
 
-  fetch("https://formsubmit.co/ajax/cc5566c6947d7f6c5bf0241e813c4926", {
-    method: "POST",
-    body: new FormData(event.target),
-  })
-    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-    .then((json) => {
-      console.log(json);
-      $contactLoader.classList.add("none");
-      $contactLoader2.classList.add("none");
-      $resp.classList.remove("none");
-      $resp2.classList.remove("none");
-      $form.reset();
-      $form2.reset();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  setTimeout(() => {
+    $contactLoader.classList.add("none");
+    $contactLoader2.classList.add("none");
+
+    $resp.classList.remove("none");
+    $resp2.classList.remove("none");
+
+    $form.reset();
+  }, 2000);
 }
 
 function handleSumbit2(event) {
   event.preventDefault();
-
-  fetch("https://formsubmit.co/ajax/cc5566c6947d7f6c5bf0241e813c4926", {
-    method: "POST",
-    body: new FormData(event.target),
-  })
-    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-    .then((json) => {
-      console.log(json);
-      window.open("https://bimformation.club.hotmart.com/login", "_self");
-    })
-
-    .catch((err) => {
-      console.log(err);
-    });
+  window.open("https://bimformation.club.hotmart.com/login", "_self");
 }
+
+// function handleSumbit(event) {
+//   event.preventDefault();
+//   const $resp = d.querySelector("#response");
+//   const $resp2 = d.querySelector("#response2");
+
+//   const $contactLoader = d.querySelector("#contact-loader");
+//   const $contactLoader2 = d.querySelector("#contact-loader2");
+
+//   $contactLoader.classList.remove("none");
+//   $contactLoader2.classList.remove("none");
+
+//   fetch("https://formsubmit.co/ajax/cc5566c6947d7f6c5bf0241e813c4926", {
+//     method: "POST",
+//     body: new FormData(event.target),
+//   })
+//     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+//     .then((json) => {
+//       console.log(json);
+//       $contactLoader.classList.add("none");
+//       $contactLoader2.classList.add("none");
+//       $resp.classList.remove("none");
+//       $resp2.classList.remove("none");
+//       $form.reset();
+//       $form2.reset();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+
+// function handleSumbit2(event) {
+//   event.preventDefault();
+
+//   fetch("https://formsubmit.co/ajax/cc5566c6947d7f6c5bf0241e813c4926", {
+//     method: "POST",
+//     body: new FormData(event.target),
+//   })
+//     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+//     .then((json) => {
+//       console.log(json);
+//       window.open("https://bimformation.club.hotmart.com/login", "_self");
+//     })
+
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
